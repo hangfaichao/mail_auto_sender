cce75d785b62e0c30b25aea6cc714bd4830439b8
wubidi <wubidi@pinduoduo.com>
2021-07-27 16:48:07 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mail_auto_sender
+
+Send email from your local markdown file automatically.
+
+Subject of mail will be the filename of markdown and content can be displayed the same as markdown.
+
+## Dependency package
+
+markdown
+
+Installation: `pip3 install markdown`
+
+## Configuration
+
+write your own 'property.conf'
--- mail_sender.py
+++ mail_sender.py
@@ -1,39 +1,108 @@
 import smtplib
 from email.mime.text import MIMEText
+import markdown
+import codecs
+from configparser import ConfigParser
+import sys
 
-#设置服务器所需信息
-#QQ邮箱服务器地址
-mail_host = 'smtp.qq.com'  
-#QQ邮箱用户名
-mail_user = 'xxxxxxxxx@qq.com'  
-#密码(部分邮箱为授权码) 
-mail_pass = 'xxxxxxxxxxxxxxxx'   
-#邮件发送方邮箱地址
-sender = 'xxxxxxxxx@qq.com'  
-#邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-receivers = ['xxxxxxxxx@qq.com']  
+class AutoMail:
 
-#设置email信息
-#邮件内容设置
-message = MIMEText('Hello, my baby!','plain','utf-8')
-#邮件主题       
-message['Subject'] = 'test' 
-#发送方信息
-message['From'] = sender 
-#接受方信息     
-message['To'] = receivers[0]  
+    def __init__(self, mail_host, mail_user, mail_pass, mail_sender, to_receivers, cc_receivers, content_type, file_path):
+        self.mail_host = mail_host
+        self.mail_user = mail_user
+        self.mail_pass = mail_pass
+        self.mail_sender = mail_sender
+        self.to_receivers = to_receivers
+        self.cc_receivers = cc_receivers
+        self.content_type = content_type
+        self.content = ""
+        self.file_path = file_path
+        self.subject = ""
 
-#登录并发送邮件
-try:
-    #连接到服务器
-    smtpObj = smtplib.SMTP_SSL(mail_host,465) 
-    #登录到服务器
-    smtpObj.login(mail_user,mail_pass) 
-    #发送
-    smtpObj.sendmail(
-        sender,receivers,message.as_string()) 
-    #退出
-    smtpObj.quit() 
-    print('success')
-except smtplib.SMTPException as e:
-    print('error',e) #打印错误
+    def markdown2html(self, text):
+        return markdown.markdown(text)
+
+    def is_markdown(self, file_path):
+        return file_path[-3:] == ".md"
+
+    def check_file_type(self, file_path):
+        if not self.is_markdown(file_path):
+            raise Exception(print("Error: File not markdown"))
+
+    def load_content(self, file_path, content_type):
+        input_file = codecs.open(file_path)
+        text = input_file.read()
+
+        content = ""
+        if content_type == "html":
+            content = self.markdown2html(text)
+        elif content_type == "plain":
+            content = text
+        else:
+            raise Exception(print("Error: CONTENT_TYPE '" + content_type + "' not support"))
+        return content
+
+    def get_subject_from_markdown_file_name(self, file_path):
+        return file_path.split("/")[-1][:-3]
+
+    def run(self):
+
+        self.check_file_type(self.file_path)
+
+        self.subject = self.get_subject_from_markdown_file_name(self.file_path)
+
+        self.content = self.load_content(self.file_path, self.content_type)
+        
+        #设置email信息
+        #邮件内容设置
+        message = MIMEText(self.content, self.content_type, 'utf-8')
+        #邮件主题       
+        message['Subject'] = self.subject
+        #发送方信息
+        message['From'] = self.mail_sender 
+        #接受方信息     
+        message['To'] = ",".join(self.to_receivers)
+        receivers = self.to_receivers
+
+        if self.cc_receivers is not None and len(self.cc_receivers) > 0:
+            message['Cc'] = ",".join(self.cc_receivers)
+            receivers += self.cc_receivers
+
+        #登录并发送邮件
+        try:
+            #连接到服务器
+            smtpObj = smtplib.SMTP_SSL(self.mail_host, 465) 
+            #登录到服务器
+            smtpObj.login(self.mail_user, self.mail_pass) 
+            #发送
+            smtpObj.sendmail(
+                self.mail_sender, receivers, message.as_string()) 
+            #退出
+            smtpObj.quit() 
+            print('Success')
+        except smtplib.SMTPException as e:
+            print('Error', e) #打印错误
+
+if __name__ == "__main__":
+
+
+    if len(sys.argv) < 2:
+        raise Exception(print("Error: please give the file path"))
+
+    file_path = sys.argv[1]
+
+    
+    config = ConfigParser()
+    config.read("property-qq.conf", encoding="utf-8")
+    mail_host = config["email"]["mail_host"]
+    mail_user = config["email"]["mail_user"]
+    mail_pass = config["email"]["mail_pass"]
+    mail_sender = config["email"]["sender"]
+    to_receivers = [receiver.strip() for receiver in config["email"]["to_receivers"].split(",")]
+    cc_receivers = [receiver.strip() for receiver in config["email"]["cc_receivers"].split(",")]
+    content_type = config["email"]["content_type"]
+    
+    auto_mail = AutoMail(mail_host, mail_user, mail_pass, mail_sender, to_receivers, cc_receivers, content_type, file_path)
+
+    auto_mail.run()
+    # send_mail(file_path, CONTENT_TYPE, mail_host, mail_user, mail_pass, sender, to_receivers, cc_receivers)
